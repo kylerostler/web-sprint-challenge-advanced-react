a31c254a43e411cffbebdfd8ffe04797d44d077d
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-10 13:20:31 -0700

    adding notes for what each piece needs to do

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -6,8 +6,8 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates (2, 2)</h3> {/** shows current coordinates */}
+          <h3 id="steps">You moved 0 times</h3> {/** counter ++ that tracks how many times the active coord have moved*/}
         </div>
         <div id="grid">
           <div className="square"></div>
@@ -23,16 +23,16 @@ export default class AppClass extends React.Component {
         <div className="info">
           <h3 id="message"></h3>
         </div>
-        <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+        <div id="keypad"> {/** each button needs functionality of changing active square changes coordinates*/}
+          <button id="left">LEFT</button>{/** x - 1, y stays */}
+          <button id="up">UP</button>{/** y - 1, x stays */}
+          <button id="right">RIGHT</button>{/** x + 1, y stays */}
+          <button id="down">DOWN</button>{/** y + 1, x stays */}
+          <button id="reset">reset</button> {/** resets square to middle 2,2 and resets count */}
         </div>
-        <form>
+        <form> {/** accepts an email and spits an error if invalid */}
           <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input id="submit" type="submit"></input> {/** submits email that was input */}
         </form>
       </div>
     )
