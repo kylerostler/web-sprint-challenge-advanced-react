4b22bcc7448bca3baad0f66ea84e20ad2c256494
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-11 14:53:22 -0700

    finished writing tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,47 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import AppFunctional from './AppFunctional'
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+beforeEach(() => {
+  render(<AppFunctional />)
+})
+
+afterEach(() => {
+  document.body.innerHTML = ''
+})
+
+test('test left button on grid', () => {
+  fireEvent.click(screen.getByText('LEFT'))
+  expect(screen.queryByText('Coordinates (1, 2)'))
+})
+
+test('test right button on grid', () => {
+  fireEvent.click(screen.getByText('RIGHT'))
+  expect(screen.queryByText('Coordinates (3, 2)'))
+})
+
+test('test reset button resets grid', () => {
+  fireEvent.click(screen.getByText('LEFT'))
+  fireEvent.click(screen.getByText('reset'))
+  expect(screen.queryByText('Coordinates (2, 2)'))
+})
+
+test('check for gridlimit messages', () => {
+  fireEvent.click(screen.getByText('LEFT'))
+  fireEvent.click(screen.getByText('LEFT'))
+  fireEvent.click(screen.getByText('LEFT'))
+  expect(screen.queryByText("You can't go left"))
+})
+
+test('steps counter operates', () => {
+fireEvent.click(screen.getByText('LEFT'))
+fireEvent.click(screen.getByText('LEFT'))
+fireEvent.click(screen.getByText('RIGHT'))
+expect(screen.queryByText('You moved 2 times')).toBeInTheDocument()
 })
