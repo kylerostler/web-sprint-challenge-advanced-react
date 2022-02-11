aeab5f54006042fd0cdb58ede40a4ebc31e55068
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-11 13:54:58 -0700

    working on refactoring class component to functional component

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,27 +1,43 @@
 import React from 'react'
 import useMoveSquare from '../hooks/CustomHooks'
+import axios from 'axios';
 
 // need state to store coordinates of active square and steps
 
+const URL = 'http://localhost:9000/api/result'
+
 export default function AppFunctional(props) {
 
-  const [resetHandler, downHandler, upHandler, rightHandler, leftHandler, state] = useMoveSquare();
+  const [resetHandler, downHandler, upHandler, rightHandler, leftHandler, state, setState] = useMoveSquare();
 
-  const {x,y,gridLimit,alert,steps} = state
+  const {x,y,steps,email,gridLimit,alert,message,submitted} = state
 
   const emailHandler = (evt) => {
-    evt.preventDefault();
-    this.setState({
-      ...this.state,
+    setState({
+      ...state,
       email: evt.target.value
     })
+  }
+
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    const payloadToSend = { x: x, y: y, steps: steps, email: email}
+    axios.post(URL, payloadToSend)
+    .then(res => {
+      console.log(res.data)
+      setState({...state, message: res.data.message, submitted: true, alert: ''})
+    }).catch(err => {
+      console.error(err.response.data.message)
+      setState({...state, message: err.response.data.message, submitted: true, alert: ''})
+    })
+    setState({...state, input: evt.target.reset()})
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates ({x}, {y})</h3>
-        <h3 id="steps">You moved {steps} times</h3>
+        {steps===1?<h3 id="steps">You moved {steps} time</h3>:<h3 id="steps">You moved {steps} times</h3>}
       </div>
       <div id="grid">
           {x===1 &&y===1?<div className="square active">B</div>:<div className="square"></div>}
@@ -35,7 +51,8 @@ export default function AppFunctional(props) {
           {x===3 &&y===3?<div className="square active">B</div>:<div className="square"></div>}
       </div>
       <div className="info">
-      {gridLimit?<h3 id="message">{alert}</h3>:<h3 id="message"></h3>}
+        {gridLimit?<h3 id="message">{alert}</h3>:<h3 id="message"></h3>}
+        {submitted?<h3 id="message">{message}</h3>:<h3 id="message"></h3>}
       </div>
       <div id="keypad">
         <button onClick={leftHandler} id="left">LEFT</button>
@@ -44,7 +61,7 @@ export default function AppFunctional(props) {
         <button onClick={downHandler} id="down">DOWN</button>
         <button onClick={resetHandler} id="reset">reset</button>
       </div>
-      <form>
+      <form id="emailform" onSubmit={onSubmit}>
         <input onChange={emailHandler} id="email" type="email" placeholder="type email"></input>
         <input id="submit" type="submit"></input>
       </form>
--- frontend/hooks/CustomHooks.js
+++ frontend/hooks/CustomHooks.js
@@ -15,66 +15,71 @@ const useMoveSquare = () => {
 
     const resetHandler = () => {
         setState(initState)
+        document.getElementById("emailform").reset();
       }
     
     const downHandler = () => {
         if(state.y === 3){
-          setState({...state,gridLimit:true,alert:"You can't go down"})
+          setState({...state,gridLimit:true,alert:"You can't go down", message: ''})
         }else{
           setState((state) => ({
             ...state,
             y: state.y + 1,
             steps: state.steps + 1,
             gridLimit: false,
-            alert: ''
+            alert: '',
+            submitted: false
           }))
         }
       }
     
     const upHandler = () => {
         if(state.y === 1){
-          setState({...state,gridLimit:true,alert:"You can't go up"})
+          setState({...state,gridLimit:true,alert:"You can't go up", message: ''})
         }else{
           setState((state) => ({
             ...state,
             y: state.y - 1,
             steps: state.steps + 1,
             gridLimit: false,
-            alert: ''
+            alert: '',
+            submitted: false
           }))
         }
       }
       
     const leftHandler = () => {
         if(state.x === 1){
-          setState({...state,gridLimit:true,alert:"You can't go left"})
+          setState({...state,gridLimit:true,alert:"You can't go left", message: ''})
         }else{
           setState((state) => ({
             ...state,
             x: state.x - 1,
             steps: state.steps + 1,
             gridLimit: false,
-            alert: ''
+            alert: '',
+            submitted: false
           }))
         }
       }
     
     const rightHandler = () => {
         if(state.x === 3){
-          setState({...state,gridLimit:true,alert:"You can't go right"})
+          setState({...state,gridLimit:true,alert:"You can't go right", message: ''})
         }else{
           setState((state) => ({
             ...state,
             x: state.x + 1,
             steps: state.steps + 1,
             gridLimit:false,
-            alert: ''
+            alert: '',
+            submitted: false
           }))
         }
       }
     
 
-      return [resetHandler, downHandler, upHandler, rightHandler, leftHandler, state]
+      return [resetHandler, downHandler, upHandler, rightHandler, leftHandler, state, setState]
 }
 
 export default useMoveSquare;
