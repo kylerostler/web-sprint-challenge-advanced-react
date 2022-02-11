ee366fc8471615d2b555df8a0fc2121cac44b9f0
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-11 10:21:36 -0700

    working on handlers for grid buttons

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,6 +1,55 @@
 import React from 'react'
 
+const URL = 'http://localhost:9000/api/result'
+const initState = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: '',
+  tooFar: false,
+  message: ''
+}
+
 export default class AppClass extends React.Component {
+  constructor(){
+    super()
+    this.state = initState
+  }
+  
+  resetHandler = () => {
+    this.setState(initState)
+  }
+
+  downHandler = () => {
+    if(this.state.y === 3){
+      this.setState({...this.state,tooFar:true,message:"You can't go down"})
+    }else{
+      this.setState((state) => ({
+        ...this.state,
+        y: state.y + 1,
+        steps: state.steps + 1,
+        tooFar: false,
+        message: ''
+      }))
+    }
+  }
+
+  upHandler = () => {
+    if(this.state.y===1){
+      this.setState({...this.state,tooFar:true,message:"You can't go up"})
+    }else{
+      this.setState((state) => ({
+        ...this.state,
+        y: state.y - 1,
+        steps: state.steps + 1,
+        tooFar: false,
+        message: ''
+      }))
+    }
+  }
+  
+
+
   render() {
     const { className } = this.props
     return (
